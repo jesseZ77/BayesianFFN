36fc6bfeb98b69e7d76ff5294d42db52ef45aa91
Jesse <zhao.j.jesse@gmail.com>
2021-01-13 09:41:04 +1100

    initial commit of results

---
+++ BayesianNetwork1.py
@@ -0,0 +1,175 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from copy import deepcopy
+
+
+class BayesianRegressionWrapper(nn.Module):
+    def __init__(self, base_model, improper=True, sigmasq=25.0, nu1=0.0, nu2=0.0):
+        super().__init__()
+        self.base_model = base_model
+        self.improper = improper
+        self.sigmasq = sigmasq
+        self.nu1 = nu1
+        self.nu2 = nu2
+        self.tausq = np.exp(np.random.normal())
+
+    def log_llk(self, X, y): # verified
+        y_num = y.numel()
+        y_pred = self(X)
+        part1 = -0.5 * y_num * np.log(2 * np.pi * self.tausq)
+        part2 = -0.5 * y_num * nn.MSELoss()(y_pred, y) / self.tausq
+        return part1 + part2
+
+    def log_prior(self): # verified
+        n_params = sum([w_.numel() for w_ in self.parameters()])
+        part0 = -0.5 * n_params * np.log(self.sigmasq)
+        part1 = -0.5 * sum([torch.sum(w_ ** 2) for w_ in list(self.parameters())]) / self.sigmasq
+        part2 = -(self.nu1 + 1) * np.log(self.tausq) - self.nu2 / self.tausq
+        return part0 + part1 + part2
+
+    def set_params(self, w): # verified
+        self.base_model.load_state_dict(w)
+
+    def set_params_from_list(self, new_w):
+        for w_, new_w_ in zip(self.parameters(), new_w):
+            w_.data = new_w_.data
+
+    def forward(self, i):
+        return self.base_model(i)
+
+    def parameters(self):
+        return self.base_model.parameters()
+
+
+class RegressionSampler:
+    def __init__(self, bayesian_model, X_train, y_train, lr=0.01, prop_sd=0.05, tausq_prop_sd=0.20):
+        self.model = bayesian_model
+        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=lr)
+        self.prop_sd = prop_sd
+        self.tausq_prop_sd = tausq_prop_sd
+
+        self.container = SampleContainerLite()
+
+        with torch.no_grad():
+            y_pred = self.model(X_train)
+            loss = nn.MSELoss()(y_pred, y_train)
+            accept = 0
+
+        self.container.append(
+            deepcopy(self.model.base_model.state_dict()), deepcopy(self.model.tausq),
+            deepcopy(y_pred), deepcopy(loss.item()), accept)
+
+    def rw_mcmc(self, X_train, y_train): # verified
+        w_k = deepcopy(self.model.base_model.state_dict())
+        tausq_k = self.model.tausq
+
+        tausq_p = self.propose_tausq(tausq_k)
+        w_p = self.propose_w(w_k)
+
+        with torch.no_grad():
+            log_post_p = self.log_posterior(X_train, y_train, w_p, tausq_p)
+            log_post_k = self.log_posterior(X_train, y_train, w_k, tausq_k)
+            log_alpha = log_post_p - log_post_k
+
+        if log_alpha > np.log(np.random.rand()):
+            self.model.set_params(w_p)
+            self.model.tausq = tausq_p
+            accept = 1
+        else:
+            self.model.set_params(w_k)
+            self.model.tausq = tausq_k
+            accept = 0
+
+        with torch.no_grad():
+            y_pred = self.model(X_train)
+            loss = nn.MSELoss()(y_pred, y_train)
+
+        self.container.append(
+            deepcopy(self.model.base_model.state_dict()), deepcopy(self.model.tausq),
+            deepcopy(y_pred), deepcopy(loss.item()), accept)
+
+    def lg_mcmc(self, X_train, y_train): # verfied
+        w_k = deepcopy(self.model.base_model.state_dict())
+        tausq_k = self.model.tausq
+
+        tausq_p = self.propose_tausq(tausq_k)
+        w_k_bar = self.compute_gradient_update(X_train, y_train, deepcopy(w_k), self.model.tausq)
+        w_p = self.propose_w(w_k_bar)
+        w_p_bar = self.compute_gradient_update(X_train, y_train, deepcopy(w_p), self.model.tausq)
+
+        with torch.no_grad():
+            log_post_p = self.log_posterior(X_train, y_train, w_p, tausq_p)
+            log_post_k = self.log_posterior(X_train, y_train, w_k, tausq_k)
+            log_prop_diff = self.log_prop_diff(w_p, w_p_bar, w_k, w_k_bar)
+            log_alpha = log_post_p - log_post_k + log_prop_diff
+
+        if log_alpha > np.log(np.random.rand()):
+            self.model.set_params(w_p)
+            self.model.tausq = tausq_p
+            accept = 1
+        else:
+            self.model.set_params(w_k)
+            self.model.tausq = tausq_k
+            accept = 0
+
+        with torch.no_grad():
+            y_pred = self.model(X_train)
+            loss = nn.MSELoss()(y_pred, y_train)
+
+        self.container.append(
+            deepcopy(self.model.base_model.state_dict()), deepcopy(self.model.tausq),
+            deepcopy(y_pred), deepcopy(loss.item()), accept)
+
+        return loss.item()
+
+    def compute_gradient_update(self, X, y, w, tausq): # verified
+        self.model.set_params(deepcopy(w))
+        self.model.tausq = tausq
+
+        # y_num = y.numel()
+        y_pred = self.model(X)
+        # sse = y_num * nn.MSELoss()(y_pred, y)
+        mse = nn.MSELoss()(y_pred, y)
+        self.optimizer.zero_grad()
+        # sse.backward()
+        mse.backward()
+        self.optimizer.step()
+
+        w_bar = deepcopy(self.model.base_model.state_dict())
+        return w_bar
+
+    @torch.no_grad()
+    def log_prop_diff(self, w_p, w_p_bar, w_k, w_k_bar): # verified
+        log_num = sum([torch.sum((x - y) ** 2) for x, y in zip(w_p_bar.values(), w_k.values())])
+        log_den = sum([torch.sum((x - y) ** 2) for x, y in zip(w_p.values(), w_k_bar.values())])
+        return -0.5 * (log_num - log_den) / self.prop_sd ** 2
+
+    @torch.no_grad()
+    def propose_w(self, w): # verified
+        w_p = {}
+        for key in w.keys():
+            w_p[key] = deepcopy(w[key]) + torch.zeros(w[key].size()).normal_(mean=0, std=self.prop_sd)
+        return w_p
+
+    def propose_tausq(self, tausq): # verified
+        tausq_p = np.exp(np.log(tausq) + np.random.normal(scale=self.tausq_prop_sd))
+        return tausq_p
+
+    @torch.no_grad()
+    def log_posterior(self, X, y, w, tausq): # verified
+        self.model.set_params(deepcopy(w))
+        self.model.tausq = tausq
+        return self.model.log_llk(X, y) + self.model.log_prior()
+
+
+class SampleContainerLite:
+    def __init__(self):
+        self.data = []
+
+    def append(self, w, tausq, y_pred_train, loss, accept):
+        w = [w_.detach() for w_ in w.values()]
+        y_pred_train = y_pred_train.detach()
+        self.data.append([w, tausq, y_pred_train, loss, accept])
+
+
---
+++ Utility.py
@@ -0,0 +1,161 @@
+import torch
+import torch.nn as nn
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def generate_linear_data(N=500, input_size=4, input_sd=1, output_size=1, output_sd=1):
+    intercept = torch.randn(1, 1)
+    w = torch.randn(input_size, output_size)
+    X = torch.randn(N, input_size) * input_sd
+    y_mean = intercept + torch.matmul(X, w)
+    y = y_mean + torch.randn(N, 1) * output_sd
+
+    return X, y, w, intercept, y_mean
+
+
+def gd(model, X, y, epochs):
+    optimizer = torch.optim.Adam(model.parameters())
+    # optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.01)
+
+    losses = []
+    criterion = nn.MSELoss()
+
+    for i in range(epochs):
+        y_pred = model(X)
+        loss = criterion(y_pred, y)
+        losses.append(loss.item())
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        if (i + 1) % 5000 == 0:
+            print(f"Done: {i + 1} / {epochs}")
+
+    return model, losses
+
+
+def generate_samples(dict_of_samplers, dict_of_algo, X, y, samples, seed=1234):
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+    for name, sampler in dict_of_samplers.items():
+        if dict_of_algo[name] == "rw":
+            for i in range(samples):
+                sampler.rw_mcmc(X, y)
+                if (i + 1) % 1000 == 0:
+                    print(f"RW sampling for {name}, done: {i + 1} / {samples}")
+
+        elif dict_of_algo[name] == "lg":
+            for i in range(samples):
+                sampler.lg_mcmc(X, y)
+                if (i + 1) % 1000 == 0:
+                    print(f"LG sampling for {name}, done: {i + 1} / {samples}")
+
+        else:
+            for i in range(samples):
+                sampler.mcmc(X, y)
+                if (i + 1) % 1000 == 0:
+                    print(f"MCMC sampling for {name}, done: {i + 1} / {samples}")
+
+
+def display_results(dict_of_samplers, burn_in, X, y, base_model):
+    # print losses, final acceptance rates
+    for name, sampler in dict_of_samplers.items():
+        final_acceptance_rate = sum([w[-1] for w in sampler.container.data]) / len(sampler.container.data)
+        print(f"Final loss, RMSE, {name}: {sampler.container.data[-1][3] ** 0.5}")
+        print(f"Final acceptance rate, {name}: {final_acceptance_rate}")
+
+    # plot loss per sample
+    plt.figure()
+    plt.title("Loss per sample, RMSE")
+    for name, sampler in dict_of_samplers.items():
+        plt.plot(np.sqrt([w[3] for w in sampler.container.data][burn_in:]), label=name)
+    plt.legend()
+    plt.show()
+
+    # plot rolling average acceptance rate
+    plt.figure()
+    plt.title("Rolling average acceptance rate")
+    for name, sampler in dict_of_samplers.items():
+        rolling_avg = pd.Series([w[-1] for w in sampler.container.data]).rolling(500).mean().dropna()
+        plt.plot(rolling_avg, label=name)
+    plt.legend()
+    plt.show()
+
+    # tausq trace plot and distribution
+    plt.figure()
+    plt.title("Trace plot: tausq")
+    for name, sampler in dict_of_samplers.items():
+        plt.plot([w[1] for w in sampler.container.data], label=name)
+    plt.legend()
+    plt.show()
+
+    plt.figure()
+    plt.title("Distribution: tausq")
+    for name, sampler in dict_of_samplers.items():
+        plt.hist([w[1] for w in sampler.container.data][burn_in:],
+                 label=name, density=True, alpha=0.5, bins=20)
+    plt.legend()
+    plt.show()
+
+    # parameter trace plot and distribution
+    for i in range(3):
+        param_len = len(list(base_model.parameters()))
+        idx1 = np.random.choice(range(param_len))
+
+        layer_shape = list(base_model.parameters())[idx1].shape
+        idx2 = np.random.choice(range(layer_shape[0]))
+
+        if len(layer_shape) > 1:
+            idx3 = np.random.choice(range(layer_shape[1]))
+
+            plt.figure()
+            plt.title(f"Trace plot: coefficient {idx1}, {idx2}, {idx3}")
+            for name, sampler in dict_of_samplers.items():
+                plt.plot([w[0][idx1][idx2, idx3].item() for w in sampler.container.data], label=name)
+            plt.legend()
+            plt.show()
+
+            plt.figure()
+            plt.title(f"Distribution: coefficient {idx1}, {idx2}, {idx3}")
+            for name, sampler in dict_of_samplers.items():
+                plt.hist([w[0][idx1][idx2, idx3].item() for w in sampler.container.data][burn_in:],
+                         label=name, density=True, alpha=0.5, bins=20)
+            plt.legend()
+            plt.show()
+
+        else:
+            plt.figure()
+            plt.title(f"Trace plot: coefficient {idx1}, {idx2}")
+            for name, sampler in dict_of_samplers.items():
+                plt.plot([w[0][idx1][idx2].item() for w in sampler.container.data], label=name)
+            plt.legend()
+            plt.show()
+
+            plt.figure()
+            plt.title(f"Distribution: coefficient {idx1}, {idx2}")
+            for name, sampler in dict_of_samplers.items():
+                plt.hist([w[0][idx1][idx2].item() for w in sampler.container.data][burn_in:],
+                         label=name, density=True, alpha=0.5, bins=20)
+            plt.legend()
+            plt.show()
+
+    # prediction trace plot and distribution
+    for i in range(5):
+        idx = np.random.choice(len(y))
+
+        plt.figure()
+        plt.title(f"Trace plot: predction {idx}")
+        for name, sampler in dict_of_samplers.items():
+            plt.plot([w[2][idx].item() for w in sampler.container.data], label=name)
+        plt.legend()
+        plt.show()
+
+        plt.figure()
+        plt.title(f"Trace plot: predction {idx}")
+        for name, sampler in dict_of_samplers.items():
+            plt.hist([w[2][idx].item() for w in sampler.container.data][burn_in:],
+                     label=name, density=True, alpha=0.5, bins=20)
+        plt.legend()
+        plt.show()
